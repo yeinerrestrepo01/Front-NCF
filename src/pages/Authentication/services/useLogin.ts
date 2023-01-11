3577619b928d72034d1f504b2000d8011fa28312
Eddi Fernando Morera Hernandez <efmorerah@gmail.com>
2023-01-11 10:36:39 -0500

    Ajuste data login

--- src/pages/Authentication/services/useLogin.ts
+++ src/pages/Authentication/services/useLogin.ts
@@ -11,21 +11,6 @@ interface LoginFetchprops {
 const login = async (params: LoginFetchprops) => {
   const data = await axios.post<IResponseGeneric<UserData>>('Autenticacion', params);
 
-  if (data?.status !== 200) {
-    return {
-      exitoso: true,
-      mensaje: '',
-      estadoHttp: 200,
-      respuesta: {
-        'employeeId': 'DO001385',
-        'samAccountName': null,
-        'emailAddress': 'Yeiner.Merino-ext@ab-inbev.com',
-        'name': 'Yeiner De Jesus Merino Restrepo',
-        'displayName': 'Yeiner De Jesus Meriño Restrepo',
-      },
-    };
-  }
-
   return data?.data;
 };
 
