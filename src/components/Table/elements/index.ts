0abd3e7e5da5b712e05625d8ccb7acd514b43c34
Eddi Fernando Morera Hernandez <efmorerah@gmail.com>
2023-01-19 10:49:45 -0500

    Delete table

--- src/components/Table/elements/index.ts
+++
@@ -1,8 +0,0 @@
-export { default as CellMoneyInput } from './CellMoneyInput/CellMoneyInput';
-export { default as CellNumberInput } from './CellNumberInput/CellNumberInput';
-export { default as CellTextInput } from './CellTextInput/CellTextInput';
-export { default as TableBody } from './TableBody/TableBody';
-export { default as TableColumn } from './TableColumn/TableColumn';
-export { default as TableHeader } from './TableHeader/TableHeader';
-export { default as TableNoRecords } from './TableNoRecords/TableNoRecords';
-export { default as TableSkeleton } from './TableSkeleton/TableSkeleton';
